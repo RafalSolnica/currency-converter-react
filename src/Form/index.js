424093cb81571b2c07a70c89aa1afbe7b390dd8f
RafalSolnica <granadag99@gmail.com>
2023-06-07 15:58:58 +0200

    updated Form component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,60 +1,46 @@
 import Buttons from "../Buttons";
+import Label from "../Label";
+import Select from "../Select";
 import "./style.css";
+
+const currencies = [
+  { id: 1, name: "PLN", value: 1 },
+  { id: 2, name: "USD", value: 4.42 },
+  { id: 3, name: "EUR", value: 4.7 },
+  { id: 4, name: "GBP", value: 5.33 },
+  { id: 5, name: "CNY", value: 0.64 },
+  { id: 6, name: "AUD", value: 2.94 },
+  { id: 7, name: "CAD", value: 3.22 },
+  { id: 8, name: "CHF", value: 4.78 },
+  { id: 9, name: "JPY", value: 0.033 },
+];
 
 const Form = () => {
   return (
-    <form action="" className="form">
+    <form className="form">
       <fieldset className="form__fieldset">
         <legend className="form__legend">Kantor walut</legend>
-        <p>
-          <label className="form__label">
-            <span className="form__text">Wybierz walutę</span>
-            <select className="form__field form__field--select">
-              <option className="form__option">PLN</option>
-              <option className="form__option">USD</option>
-              <option className="form__option">EUR</option>
-              <option className="form__option">GBP</option>
-              <option className="form__option">CNY</option>
-              <option className="form__option">AUD</option>
-              <option className="form__option">CAD</option>
-              <option className="form__option">CHF</option>
-              <option className="form__option">JPY</option>
-            </select>
-          </label>
-        </p>
 
-        <p>
-          <label className="form__label">
-            <span className="form__text">
-              Podaj wartość<span className="form__required">*</span>
-            </span>
+        <Label
+          text="Wybierz walutę"
+          content={<Select currencies={currencies} />}
+        />
+
+        <Label
+          text="Podaj wartość*"
+          content={
             <input type="number" className="form__field" inputMode="numeric" />
-          </label>
-        </p>
+          }
+        />
 
-        <p>
-          <label className="form__label">
-            <span className="form__text">Wybierz walutę</span>
-            <select className="form__field form__field--select">
-              <option className="form__option">PLN</option>
-              <option className="form__option">USD</option>
-              <option className="form__option">EUR</option>
-              <option className="form__option">GBP</option>
-              <option className="form__option">CNY</option>
-              <option className="form__option">AUD</option>
-              <option className="form__option">CAD</option>
-              <option className="form__option">CHF</option>
-              <option className="form__option">JPY</option>
-            </select>
-          </label>
-        </p>
+        <Label
+          text="Wybierz walutę"
+          content={<Select currencies={currencies} />}
+        />
 
         <Buttons />
 
-        <p>
-          Pola oznaczone symbolem <span className="form__required">*</span> są
-          wymagane.
-        </p>
+        <span>Pola oznaczone symbolem * są wymagane.</span>
       </fieldset>
     </form>
   );
