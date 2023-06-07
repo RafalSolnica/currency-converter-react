558b8bbcb8ffab1adbe3c66724b9cc013d191926
RafalSolnica <granadag99@gmail.com>
2023-06-07 16:03:44 +0200

    added Label component

---
+++ src/Label/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const Label = ({ text, content }) => {
+  return (
+    <label className="form__label">
+      <span>{text}</span>
+      {content}
+    </label>
+  );
+};
+
+export default Label;
