aeaa42cafd2c670103f0102465e38295341a266f
RafalSolnica <granadag99@gmail.com>
2023-06-10 14:35:44 +0200

    moved Result component out of the Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Container from "./Container";
 import Form from "./Form";
+import Result from "./Result";
+import { useState } from "react";
 
 function App() {
+  const [initialCurrency, setInitCurrency] = useState("PLN");
+  const [amount, setAmount] = useState(0);
+  const [convertedCurrency, setConvertedCurrency] = useState("USD");
+  const [hideResult, setHideResult] = useState(true);
+  const [resultValue, setResultValue] = useState(0);
+
   return (
     <Container>
-      <Form />
+      <Form
+        initialCurrency={initialCurrency}
+        setInitCurrency={setInitCurrency}
+        amount={amount}
+        setAmount={setAmount}
+        convertedCurrency={convertedCurrency}
+        setConvertedCurrency={setConvertedCurrency}
+        setResultValue={setResultValue}
+        setHideResult={setHideResult}
+      />
+      <Result
+        hideResult={hideResult}
+        resultValue={resultValue}
+        convertedCurrency={convertedCurrency}
+      />
     </Container>
   );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -2,28 +2,19 @@ import { useState } from "react";
 import Buttons from "../Buttons";
 import Label from "../Label";
 import Select from "../Select";
-import Result from "../Result";
+import { currencies } from "../currencies";
 import "./style.css";
 
-const Form = () => {
-  const currencies = [
-    { id: 1, name: "PLN", value: 1 },
-    { id: 2, name: "USD", value: 4.42 },
-    { id: 3, name: "EUR", value: 4.7 },
-    { id: 4, name: "GBP", value: 5.33 },
-    { id: 5, name: "CNY", value: 0.64 },
-    { id: 6, name: "AUD", value: 2.94 },
-    { id: 7, name: "CAD", value: 3.22 },
-    { id: 8, name: "CHF", value: 4.78 },
-    { id: 9, name: "JPY", value: 0.033 },
-  ];
-
-  const [initialCurrency, setInitCurrency] = useState("PLN");
-  const [amount, setAmount] = useState(0);
-  const [convertedCurrency, setConvertedCurrency] = useState("USD");
-  const [hideResult, setHideResult] = useState(true);
-  const [resultValue, setResultValue] = useState(0);
-
+const Form = ({
+  initialCurrency,
+  setInitCurrency,
+  amount,
+  setAmount,
+  convertedCurrency,
+  setConvertedCurrency,
+  setResultValue,
+  setHideResult,
+}) => {
   const handleInput = ({ target }) => {
     setAmount(target.value);
   };
@@ -57,7 +48,6 @@ const Form = () => {
           text="Wybierz walutę"
           content={
             <Select
-              currencies={currencies}
               selectedCurrency={initialCurrency}
               method={setInitCurrency}
             />
@@ -84,7 +74,6 @@ const Form = () => {
           text="Wybierz walutę"
           content={
             <Select
-              currencies={currencies}
               selectedCurrency={convertedCurrency}
               method={setConvertedCurrency}
             />
@@ -95,11 +84,6 @@ const Form = () => {
 
         <span>Pola oznaczone symbolem * są wymagane.</span>
       </fieldset>
-      <Result
-        hideResult={hideResult}
-        resultValue={resultValue}
-        convertedCurrency={convertedCurrency}
-      />
     </form>
   );
 };
