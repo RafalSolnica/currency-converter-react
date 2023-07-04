285742f60bb95625f6b9d3d39995aca4853fa5a4
RafalSolnica <granadag99@gmail.com>
2023-07-04 13:11:55 +0200

    implemented styled-components into Container component

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,8 +1,10 @@
-.container {
+import { styled } from "styled-components";
+
+export const StyledContainer = styled.main`
   color: #ccc;
   font-family: "Roboto", sans-serif;
   max-width: 600px;
   margin: 0 auto;
   display: flex;
   flex-flow: column;
-}
+`;
