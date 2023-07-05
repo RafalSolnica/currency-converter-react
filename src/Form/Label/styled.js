14262bb2b58472a2387d27989a56930e913b83da
RafalSolnica <granadag99@gmail.com>
2023-07-06 01:03:37 +0200

    deleted Label and Buttons components

--- src/Form/Label/styled.js
+++
@@ -1,9 +0,0 @@
-import { styled } from "styled-components";
-
-export const StyledLabel = styled.label`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 20px;
-  text-align: right;
-  margin: 15px 0;
-`;
