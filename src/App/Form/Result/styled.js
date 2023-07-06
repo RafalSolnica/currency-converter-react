09b33d57ffffdafb9598728f2c9788aeda80ac95
RafalSolnica <granadag99@gmail.com>
2023-07-06 02:50:09 +0200

    added styled component Text isnide Result

--- src/App/Form/Result/styled.js
+++ src/App/Form/Result/styled.js
@@ -3,10 +3,13 @@ import { styled, css } from "styled-components";
 export const Wrapper = styled.p`
   margin: 20px 5%;
   font-weight: bold;
-  background: ${({ theme }) => theme.color.transparentGrey};
   ${({ $hideResult }) =>
     $hideResult &&
     css`
       display: none;
     `};
+`;
+
+export const Text = styled.span`
+  background: ${({ theme }) => theme.color.transparentGrey};
 `;
